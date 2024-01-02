0a008e10b09c18f555d0b0e820a20a6e3f60914f
JooYoung2274 <jooooo2274@gmail.com>
2024-01-02 16:53:11 +0900

    my cookies set & article posting

--- app.ts
+++ app.ts
@@ -3,10 +3,8 @@ import { articlePost, extractCookies } from './src/data-parsing'
 const app = express()
 
 app.get('/', async (req, res) => {
-   const a = await extractCookies()
-  const result=  await articlePost(a)
-  console.log(result)
-    res.send('Hello World!')
+  const myCookies = await extractCookies()
+  await articlePost(myCookies)
 })
 
 app.listen(3333,() => {
--- src/data-parsing.ts
+++ src/data-parsing.ts
@@ -1,20 +1,26 @@
 import axios from 'axios';
 import puppeteer from 'puppeteer';
+import * as fs from 'fs';
+
+export class TistoryUpload{
+
+  async extractCookies() {}
+}
 
 export async function extractCookies() {
   const browser = await puppeteer.launch({headless: false});
   const page = await browser.newPage();
 
   // 웹 페이지 열기
-  await page.goto('https://joorrr.tistory.com/manage');
+  await page.goto('https://티스토리아이디.tistory.com/manage');
 
   // 로그인
   await page.click('#cMain > div > div > div > a.btn_login.link_kakao_id > span.txt_login'); // 버튼 클릭
   await page.waitForSelector('#loginId--1'); // 페이지 로딩 대기
   await page.focus('#loginId--1'); // 로그인 input 포커스
-  await page.keyboard.type('개인아이디!!!!!!!!!!!!!!!!!!!!!'); // 아이디 입력
+  await page.keyboard.type('개인아이디'); // 아이디 입력
   await page.focus('#password--2'); // 비밀번호 input 포커스
-  await page.keyboard.type('개인비밀번호!!!!!!!!!!!!!!!!!!!!!!'); // 비밀번호 입력
+  await page.keyboard.type('개인비밀번호'); // 비밀번호 입력
   await page.click('#mainContent > div > div > form > div.confirm_btn > button.btn_g.highlight.submit') // 로그인 버튼 클릭
  await page.waitForNavigation(); // 페이지 로딩 대기
   // 쿠키 가져오고
@@ -27,17 +33,20 @@ export async function extractCookies() {
 
 // @ts-ignore
 export async function articlePost(cookies){
+  const domain = './src/README.md' // 포스팅할 아티클 경로
+  const article = fs.readFileSync(domain, 'utf8')
+
     // 해당 쿠키 + 포스팅 할 내용으로 POST 요청
-  const a = {
+  const uploadData = {
     "id": "0",
-    "title": "ㅇㄴㄹㅁㄴㅇ",
-    "content": "<p data-ke-size=\"size16\">ㄹㅁㄴㅇㄹㄴㅁㅇㄹ</p>",
-    "slogan": "ㅇㄴㄹㅁㄴㅇ",
+    "title": "testesetsetest",
+    "content": article,
+    "slogan": "ㄴㅇㄹㅁㄴㅇㄹㅇㅁㄴㄹㅁㄴㅇㄹ",
     "visibility": 0,
-    "category": 1045846,
+    "category": 1,
     "tag": "",
     "published": 1,
-    "password": "40NzUxMj",
+    "password": "43NjI2Mz",
     "uselessMarginForEntry": 1,
     "daumLike": "401",
     "cclCommercial": 0,
@@ -49,7 +58,7 @@ export async function articlePost(cookies){
     "draftSequence": null
   }
     try {
-      const response = await axios.post('https://joorrr.tistory.com/manage/posts.json', a, {
+      const response = await axios.post(`https://티스토리아이디주소.tistory.com/manage/posts.json`, uploadData, {
         headers: {
           'Content-Type': 'application/json',
           // @ts-ignore
@@ -57,7 +66,7 @@ export async function articlePost(cookies){
         }
       });
   
-      // 응답 처리
+  //     // 응답 처리
       return response.data;
       // @ts-ignore
     } catch (error) {
